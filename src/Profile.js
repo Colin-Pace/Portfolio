43ef45144e7ff42e6eeb7aac553b65b0fd334c11
Colin Pace <colinpace1987@gmail.com>
2023-01-26 07:41:18 -0600

    edited profile

--- src/Profile.js
+++ src/Profile.js
@@ -4,9 +4,8 @@ import './Profile.css';
 function Profile() { 
   return ( 
     <div> 
-      <p id = 'firstPara' className = 'profileText'>I am an Austin based web developer specializing in React applications. I have a certificate from The Coding Boot Camp at The University of Texas at Austin. </p>
-      <p className = 'profileText'>I have studied web development for six years. The projects section of this portfolio website showcases a few recent projects that demonstrate my React skills.</p>
-      <p id = 'lastText' className = 'profileText'>In addition to React, I have studied Python and the Django and Flask frameworks as well as Postgres and Express.</p>
+      <p className = 'profileText'>I am intelligent and educated. My intelligence is attested by the Graduate Record Examinations (GRE): I placed in the 96th percentile nationally in the US in both the subjects of verbal reasoning and analytical writing - that is the top four percent. My education is evidenced by my BA and MA degrees and a coding certificate which I received from the University of Texas at Austin (UT); a world class university, UT was recently ranked 43rd among universities globally and 23rd in the nation. Furthermore, only 13% of citizens in the US have a Master’s degree or higher.</p>
+      <p id = 'lastText' className = 'profileText'>In terms of work experience, the two titles I have had are Research and Machine Intelligence Analyst and also Research Engineering Scientist Associate. From the titles, it is clear that my work experience is in STEM subjects which is a passion of mine. As an analyst, I analyzed machine intelligence responses to user prompts and created improvements which the machine was then trained on. As an engineering scientist, I performed both manual and automated testing of software, and we wrote the automated tests in Cypress. Both experiences gave me insight into working in the STEM fields and prepared me for further related work.</p>
     </div>
   );
 }
