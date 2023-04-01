a1bb5c6b8e477509c5d38b1f055b6e87fc120617
Colin Pace <colinpace1987@gmail.com>
2023-04-01 16:51:19 -0500

    textual edits

--- src/Profile.js
+++ src/Profile.js
@@ -5,7 +5,7 @@ function Profile() {
   return ( 
     <div> 
       <p className = 'profileText'>In terms of work, I have had the title Research Engineering Scientist Associate. As an engineering scientist, I performed both manual and automated testing of software.</p>
-      <p id = 'lastText' className = 'profileText'>I have a BA and MA degree and a coding certificate from The University of Texas at Austin (UT)</p>
+      <p id = 'lastText' className = 'profileText'>I have a BA and MA degree and a coding certificate from The University of Texas at Austin.</p>
     </div>
   );
 }
