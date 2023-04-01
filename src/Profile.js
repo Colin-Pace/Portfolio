e68561991a42ddddf320e395cc2cc8e0b296428f
Colin Pace <colinpace1987@gmail.com>
2023-04-01 16:48:54 -0500

    textual updates

--- src/Profile.js
+++ src/Profile.js
@@ -4,8 +4,8 @@ import './Profile.css';
 function Profile() { 
   return ( 
     <div> 
-      <p className = 'profileText'>I am experienced, intelligent, and educated. In terms of work experience, the title I have had is Research Engineering Scientist Associate. As an engineering scientist, I performed both manual and automated testing of software, and I wrote the automated tests in Cypress.</p>
-      <p id = 'lastText' className = 'profileText'>My intelligence is attested by the Graduate Record Examinations (GRE): I placed in the 96th percentile nationally in the US in both the subjects of verbal reasoning and analytical writing - that is the top four percent. My education is evidenced by my BA and MA degrees and a coding certificate which I received from the University of Texas at Austin (UT); a world-class university, UT was recently ranked 43rd among universities globally and 23rd in the nation. Furthermore, only 13% of citizens in the US have a Master’s degree or higher.</p>
+      <p className = 'profileText'>In terms of work, I have had the title Research Engineering Scientist Associate. As an engineering scientist, I performed both manual and automated testing of software.</p>
+      <p id = 'lastText' className = 'profileText'>I have a BA and MA degree and a coding certificate from The University of Texas at Austin (UT)</p>
     </div>
   );
 }
