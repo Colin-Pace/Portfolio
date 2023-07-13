ac8355aa6207d1b9afc2cf24c383ea6bdbc58214
Colin Pace <colinpace1987@gmail.com>
2023-07-12 22:04:42 -0500

    textual edit

--- src/Profile.js
+++ src/Profile.js
@@ -4,7 +4,7 @@ import './Profile.css';
 function Profile() { 
   return ( 
     <div> 
-      <p className = 'profileText'>I live in Austin where I was a student at the University of Texas at Austin (UT) from 2006 - 2017. During my time as a student, I also traveled internationally, and I studied a few languages. In terms of work, I am a project coordinator at a business based in San Diego. At my previous job, I had the title Research Engineering Scientist Associate, and I performed both manual and automated testing of software.</p>
+      <p className = 'profileText'>I live in Austin where I was a student at the University of Texas at Austin (UT) from 2006 - 2017. During my time as a student, I also traveled internationally, and I studied a few languages. In terms of work, I am a project coordinator at a business based in San Diego.</p>
       <p id = 'lastText' className = 'profileText'>I have a BA and MA degree and a coding certificate from UT and a certificate for project management from Google.</p>
     </div>
   );
