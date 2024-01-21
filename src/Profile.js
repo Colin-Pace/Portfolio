bb536a8adcfee9bbe218d413d9d01b8771bf706e
Colin Pace <colinpace1987@gmail.com>
2024-01-21 14:58:59 -0600

    textual edits

--- src/Profile.js
+++ src/Profile.js
@@ -4,7 +4,7 @@ import './Profile.css';
 function Profile() { 
   return ( 
     <div> 
-      <p className = 'profileText'>A discerning science blogger, unwavering vegan, and ardent advocate for the Democrats, I have placed nationally in the 99th and 96th percentiles on standardized tests, each time in two subjects. I have a BA in Anthropology, MA in Asian Studies with a focus on Modern Standard Hindi, and a coding boot camp certificate from The University of Texas at Austin, one of the top 50 universities in the world. Focused on the intricate realm of molecular genetics and proficient in coding web apps and data structures and algorithms, I am currently engrossed in the development of a theory of gene therapy for cancer and the exploration of artificial neural networks, learning about the dynamic domains of science and technology.</p>
+      <p className = 'profileText'>A discerning science blogger and unwavering vegan, I have placed nationally in the 99th and 96th percentiles on standardized tests, each time in two subjects. I have a BA in Anthropology, MA in Asian Studies with a focus on Modern Standard Hindi, and a coding boot camp certificate from The University of Texas at Austin, one of the top 50 universities in the world. Focused on the intricate realm of molecular genetics and proficient in coding web apps, data structures, and algorithms, I am currently engrossed in the development of a theory of gene therapy for cancer and the exploration of artificial neural networks, learning about the dynamic domains of science and technology.</p>
     </div>
   );
 }
