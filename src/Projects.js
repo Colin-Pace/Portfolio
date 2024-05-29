b10f06cc1c5ecb48b72ad7fcd2573d21cab27041
Colin Pace <colinpace1987@gmail.com>
2024-05-28 19:57:08 -0500

    updated description of Gem Search

--- src/Projects.js
+++ src/Projects.js
@@ -11,7 +11,7 @@ function Projects() {
         />
       </div>  
       <h3 className = 'imageTitle'>Gem Search</h3>    
-      <p className = 'imageText'>Gem Search is a front end game that has a player use the arrow keys to move a green image around the screen to collect the blue image and to do so without touching the orange images. There is a levels option for easy and hard. Also there is a points tabby that keeps track of one's points. Gem Search uses object oriented design, with classes for the board, player, gem, and opponents (called "traffic" after the frogger game). Gem Search might be thought of as a combination of parts of the games of Frogger and Snake.</p>
+      <p className = 'imageText'>Designed specifically for desktops, Gem Search is a front end game that has a player use the arrow keys to move a green image around the screen to collect the blue image and to do so without touching the orange images. There is a levels option for easy and hard. Also there is a points tabby that keeps track of one's points. Gem Search uses object oriented design, with classes for the board, player, gem, and opponents (called "traffic" after the frogger game). Gem Search might be thought of as a combination of parts of the games of Frogger and Snake.</p>
       <div className = 'imageLinks'>
         <a 
           href="https://colin-pace.github.io/Gem-Search/"
