6b133d2e85c879c3977b71ee17fcab5dcd9a421e
Colin Pace <colinpace1987@gmail.com>
2024-06-06 16:50:02 -0500

    textual changes

--- src/Tests.js
+++ src/Tests.js
@@ -5,7 +5,7 @@ function Tests() {
   return (
     <div id = "testSection">
       <h1>Percentile Rankings on National and State Tests</h1>
-      <p>Throughout my early and young adult life, I placed well on standardized tests. Here are six tests in which I placed in the 95th percentile or higher, with a few placements in the 1st percentile. The format of the list is the test name, my percentile rankings on them, and my age during the test.
+      <p>Throughout my early and young-adult life, I placed well on standardized tests. Here are six tests in which I placed in the 95th percentile or higher, with at least 5 placements in the 1st percentile (and possibly as many as 10 placements in the 1st percentile). The format of the list is the test name, my percentile rankings on it, and my age during the test.
         <ol>
           <li><a href = "#testOneTitle">The Stanford Early School Achievement Test, 98th percentile (composite), age 5</a></li>
           <li><a href = "#testTwoTitle">The Metropolitan Readiness Tests, 95th percentile (composite), age 6</a></li>
@@ -14,7 +14,7 @@ function Tests() {
           <li><a href = "#testFiveTitle">The Texas Assessment of Academic Skills, 99th* (reading), 98th* (mathematics), and 99th* (science) percentiles, age 14</a></li>
           <li><a href = "#testSixTitle">The Graduate Record Examinations, 96th+ (verbal reasoning) and 96th+ (analytical writing) percentile, age 22</a></li>
         </ol>
-      Key: * denotes ChatGPT's estimation percentile based on the test result form, and + denotes a likely higher percentile by age range 
+      Key: * denotes ChatGPT's estimation percentile based on the test result form, and + denotes a likely higher percentile by age range.
       </p>
       
       <h2 id = "testOneTitle">The Stanford Test 1992</h2>
@@ -63,7 +63,7 @@ function Tests() {
         />
 
       <h2 id = "testSixTitle">The Graduate Record Examinations (GRE) 2009</h2>
-        <p><b>96th+ (verbal reasoning) and 96th+ (analytical writing) percentiles</b>: The GRE was an interesting test for me for a number of reasons. It's the last standardized test that I took. Also, I didn't study or try on the mathematics section, which is why I received a much lower placement. Perhaps most noteworthy, after the two 96th percentile placements in verbal reasoning and analytical writing, is the age at which I tested: 22. Today, the average test taker is 24 years old, with 50% of test takers older than 22. If this was true when I took the test, then for my age range, I probably placed higher, perhaps even again in the top percentile for both categories. The testing service, ETS, doesn't give statistics, so the status of the percentiles remains unclear.</p>
+        <p><b>96th+ (verbal reasoning) and 96th+ (analytical writing) percentiles</b>: The GRE was an interesting test for me for a number of reasons. It's the last standardized test that I took. Also, I didn't study or try on the mathematics section, which is why I received a much lower placement. Perhaps most noteworthy after the two 96th percentile placements in verbal reasoning and analytical writing is the age at which I tested: 22. Today, the average test taker is 24 years old, with 50% of test takers older than 22. If this was true when I took the test, then for my age range, I probably placed higher, perhaps even again in the top percentile for both categories. The testing service, ETS, doesn't give statistics, so the status of the percentiles remains unclear.</p>
         <img 
           id = 'gre' 
           className = '' 
