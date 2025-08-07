2b77b8195bc302f770dd771cfb56214426f43c58
Colin Pace <colinpace@mac.attlocal.net>
2025-08-06 21:56:48 -0500

    removed apps

--- src/Projects.js
+++ src/Projects.js
@@ -11,7 +11,7 @@ function Projects() {
         />
       </div>  
       <h3 className = 'imageTitle'>Gem Search</h3>    
-      <p className = 'imageText'>Designed specifically for desktops, Gem Search is a front end game that has a player use the arrow keys to move a green image around the screen to collect the blue image and to do so without touching the orange images. There is a levels option for easy and hard. Also there is a points tabby that keeps track of one's points. Gem Search uses object oriented design, with classes for the board, player, gem, and opponents (called "traffic" after the frogger game). Gem Search might be thought of as a combination of parts of the games of Frogger and Snake.</p>
+      <p className = 'imageText'>Designed specifically for desktops, Gem Search is a front-end game that has a player use the arrow keys to move a green image around the screen and collect the blue image for points. If the green image touches an orange image, the game ends. There is an option for an easy or hard level. Also there is a score that keeps track of points. Gem Search uses object oriented design, with classes for the board, player, gem, and opponents (called "traffic" after the frogger game). Gem Search can be thought of as a combination of Frogger and Snake.</p>
       <div className = 'imageLinks'>
         <a 
           href="https://colin-pace.github.io/Gem-Search/"
@@ -23,24 +23,6 @@ function Projects() {
         >GitHub</a>
       </div>
 
-      <div className = 'projectImageContainer'>
-        <img 
-          className = 'projectImage'
-          src = {require('./outerSpace.png')}
-        />
-      </div>  
-      <h3 className = 'imageTitle'>Outer Space Invaders</h3>    
-      <p className = 'imageText'>Also designed for desktops, Outer Space Invaders tests a player's ability to launch photon torpedos toward the invading Borg ships. The ships do not launch back, but if they touch the plalyer or the bottom of the board, then the game ends. The game ends also if the player destroys all the borg ships. A front-end game with object-oriented design, Outer Space Invaders challenges a player's intuition and hand-eye coordination.</p>
-      <div className = 'imageLinks'>
-        <a 
-          href="https://colin-pace.github.io/Outer-Space-Invaders/"
-          target="_blank"
-        >Live app</a><br/><br/>
-        <a 
-          href="https://github.com/Colin-Pace/Outer-Space-Invaders"
-          target="_blank" 
-        >GitHub</a>
-      </div>
        
       <div className = 'projectImageContainer'>
         <img 
@@ -49,7 +31,7 @@ function Projects() {
         />
       </div>  
       <h3 className = 'imageTitle'>Notes Organizer</h3>    
-      <p className = 'imageText'>Notes organizer is a front end application that enables the user to create, read, update, and delete notes. Another feature of the application is the sorting option, which can sort the notes according to newest to oldest or oldest to newest. The technology used in the application is described in the GitHub ReadMe, which is linked to below.</p>
+      <p className = 'imageText'>Notes organizer is a front-end application that enables a user to create, read, update, and delete notes. Another feature of the application is a sorting option, which can sort the notes according to newest to oldest or oldest to newest. The technology used in the application is described in the GitHub ReadMe, which is linked below.</p>
       <div className = 'imageLinks'>
         <a 
           href="https://colin-pace.github.io/Notes/"
@@ -61,24 +43,6 @@ function Projects() {
         >GitHub</a>
       </div>
       
-      <div className = 'projectImageContainer'>
-        <img 
-          className = 'projectImage'
-          src = {require('./twitter.png')}
-        />
-      </div>
-      <h3 className = 'imageTitle'>Twitter Clone</h3>
-      <p className = 'imageText'>The Twitter Clone is a front end application in React that allows a user to register an account and log in to the website. Once logged in, the user can make Tweets and comment on Tweets. Other features include the ability to view the user's profile page and the profile pages of other users. On other users' profile pages, a user can follow them and then see those users' tweets in the user's home page. The technology used in the application is described in the ReadMe on GitHub.</p>
-      <div className = 'imageLinks'>
-        <a 
-          href="https://colin-pace.github.io/TwitterClone/"
-          target="_blank"
-        >Live app</a><br/><br/>
-        <a 
-          href="https://github.com/Colin-Pace/TwitterClone" 
-          target="_blank"
-        >GitHub</a>
-      </div>
 
       <div className = 'projectImageContainer'>
         <img 
@@ -87,7 +51,7 @@ function Projects() {
         />
       </div>
       <h3 className = 'imageTitle'>Dijkstra's Algorithm Illustrated</h3>
-      <p className = 'imageText'>A vanilla JavaScript game that illustrates Dijkstra's algorithm. The game is to use the up, down, left, and right keys to move steps away from the green block that follows the user's blue block. The path that the green block takes is determined by Dijkstra's algorithm. The board is a wrap around board, which enables the user to move from side to side relatively easily.</p>
+      <p className = 'imageText'>This is a vanilla JavaScript game that illustrates Dijkstra's algorithm. The game is to use the up, down, left, and right keys to move steps away from the green block that follows the user's blue block. The path that the green block takes is determined by Dijkstra's algorithm and is illustrated with orange blocks. The board is a wrap around board, which enables the user to move easily from either side of the board to the other.</p>
       <div className = 'imageLinks'>
         <a 
           href="https://colin-pace.github.io/Dijkstra-Illustrated/"
