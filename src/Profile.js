f0b36c5e3ef769e81a35f1a34042ebb3b9cc8b88
Colin Pace <colinpace@mac.attlocal.net>
2025-08-10 13:56:23 -0500

    edit to text of profile

--- src/Profile.js
+++ src/Profile.js
@@ -8,7 +8,7 @@ function Profile() {
         id = 'portrait'
         src = {require('./portrait.jpg')}
       />
-      <p id = 'firstParagraph' className = 'lastText'>I am a Geographic Information Systems (GIS) analyst with a background in anthropology, South Asian languages, and science, technology, engineering, and mathematics (STEM). I bring a combination of analytical rigor, cultural fluency, and technical acumen to my work. My intellectual interests range from current science and ethical technology, to environmentalism and veganism. I am committed to cultivating a mindful and erudite life.</p>
+      <p id = 'firstParagraph' className = 'lastText'>I am a Geographic Information Systems (GIS) analyst with a background in anthropology, South Asian languages, and science, technology, engineering, and mathematics (STEM). I bring a combination of analytical rigor, cultural fluency, and technical acumen to my work. My intellectual interests range from science and ethical technology, to environmentalism and veganism. I am committed to cultivating a mindful and erudite life.</p>
     </div>
   );
 }
