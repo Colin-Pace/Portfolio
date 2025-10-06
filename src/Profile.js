a9c2ff63d899e09b28cc653f5f57203f555e0350
Colin Pace <colinpace@mac.attlocal.net>
2025-10-06 16:34:18 -0500

    changed image

--- src/Profile.js
+++ src/Profile.js
@@ -6,7 +6,7 @@ function Profile() {
     <div>
       <img
         id = 'portrait'
-        src = {require('./portrait.jpg')}
+        src = {require('./selfPortrait.jpg')}
       />
       <p id = 'firstParagraph' className = 'lastText'>I am a Geographic Information Systems (GIS) Analyst with a background in anthropology, South Asian languages, and science, technology, engineering, and mathematics (STEM). I bring a combination of analytical rigor, cultural fluency, and technical acumen to my work. My intellectual interests range from science and technology, to environmentalism and plant-based diets. I am committed to cultivating a mindful and erudite life.</p>
     </div>
