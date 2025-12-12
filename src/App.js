b3831b9ac0cc212b810defa545ef72c7276e1814
Colin Pace <colinpace@Colins-MacBook-Air.local>
2025-12-12 05:33:15 -0600

    added photo and autobio link

--- src/App.js
+++ src/App.js
@@ -49,6 +49,14 @@ function App() {
             id = {display === 'projects' ? 'selectedProjectsTitle' : 'projectsTitle'}
           >Code</a>
 
+          <a 
+            id = 'autobioLink'
+            href = {require('./SuperlativeIntelligence.pdf')}
+            target="_blank"
+          >
+            Autobiography
+          </a>
+
           <a
             href="https://medium.com/@colinpace1987" 
             target="_blank" 
--- src/Profile.js
+++ src/Profile.js
@@ -4,6 +4,12 @@ import './Profile.css';
 function Profile() { 
   return ( 
     <div>
+
+     
+      <img
+        id = 'portrait'
+        src = {require('./SelfPortrait.jpg')}
+      />
       
       <p id = 'firstParagraph'>I am exploring a campaign to become president of the United States of America. I have a potentially globally and historically unprecedented testing profile. I have theorized and, as president, would develop a treatment for disease with genetic engineering, and I plan to negotiate with the possible medical advancements for world peace. I have other campaign goals. To read about them and more about me, please click the link at the top of the page for my autobiography.</p>
       <p id = 'lastText'>Professionally, I am a Geographic Information Systems Analyst with a background in anthropology, South Asian languages, and science, technology, engineering, and mathematics. I bring a combination of analytical rigor, cultural fluency, and technical acumen to my work. My intellectual interests range from science and technology, to environmentalism and plant-based diets. I am committed to cultivating a mindful and erudite life.</p>
